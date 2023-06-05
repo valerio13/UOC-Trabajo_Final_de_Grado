9a4014edf5c023d23c9f32e5b5ae83a5d68fe58f
Valerio <valerio.colantonio@gmail.com>
2023-06-05 18:06:20 +0200

    Test MQTT: server setup

--- Modulo IoT/Test Mqtt/src/main.cpp
+++ Modulo IoT/Test Mqtt/src/main.cpp
@@ -1,14 +1,7 @@
+#include "networkConfig.h"
 #include <PubSubClient.h>
 #include <WiFi.h>
 #include <Wire.h>
-
-// Replace the next variables with your SSID/Password combination
-const char *ssid = "REPLACE_WITH_YOUR_SSID";
-const char *password = "REPLACE_WITH_YOUR_PASSWORD";
-
-// Add your MQTT Broker IP address, example:
-// const char* mqtt_server = "192.168.1.144";
-const char *mqtt_server = "YOUR_MQTT_BROKER_IP_ADDRESS";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -16,103 +9,102 @@ long lastMsg = 0;
 char msg[50];
 int value = 0;
 
-float temperature = 0;
 float humidity = 0;
 
 void setup_wifi();
 void callback(char *topic, byte *message, unsigned int length);
 
-void setup()
-{
-  Serial.begin(115200);
+void setup() {
+  Serial.begin(921600);
 
   setup_wifi();
-  client.setServer(mqtt_server, 1883);
+  client.setServer(MQTT_SERVER, 1883);
   client.setCallback(callback);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_NAME);
+
+  WiFi.begin(WIFI_NAME, PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
 
-  void setup_wifi() {
-    delay(10);
-    // We start by connecting to a WiFi network
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
-    WiFi.begin(ssid, password);
+void callback(char *topic, byte *message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
 
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
   }
+  Serial.println();
 
-  void callback(char *topic, byte *message, unsigned int length) {
-    Serial.print("Message arrived on topic: ");
-    Serial.print(topic);
-    Serial.print(". Message: ");
-    String messageTemp;
+  // Feel free to add more if statements to control more GPIOs with MQTT
 
-    for (int i = 0; i < length; i++) {
-      Serial.print((char)message[i]);
-      messageTemp += (char)message[i];
-    }
-    Serial.println();
-
-    // Feel free to add more if statements to control more GPIOs with MQTT
-
-    // If a message is received on the topic esp32/output, you check if the
-    // message is either "on" or "off". Changes the output state according to
-    // the message
-    if (String(topic) == "esp32/output") {
-      Serial.print("Changing output to ");
-      if (messageTemp == "on") {
-        Serial.println("on");
-      } else if (messageTemp == "off") {
-        Serial.println("off");
-      }
+  // If a message is received on the topic esp32/output, you check if the
+  // message is either "on" or "off". Changes the output state according to
+  // the message
+  if (String(topic) == "esp32/output") {
+    Serial.print("Changing output to ");
+    if (messageTemp == "on") {
+      Serial.println("on");
+    } else if (messageTemp == "off") {
+      Serial.println("off");
     }
   }
+}
 
-  void reconnect() {
-    // Loop until we're reconnected
-    while (!client.connected()) {
-      Serial.print("Attempting MQTT connection...");
-      // Attempt to connect
-      if (client.connect("ESP8266Client")) {
-        Serial.println("connected");
-        // Subscribe
-        client.subscribe("esp32/output");
-      } else {
-        Serial.print("failed, rc=");
-        Serial.print(client.state());
-        Serial.println(" try again in 5 seconds");
-        // Wait 5 seconds before retrying
-        delay(5000);
-      }
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(ID, USER, PASSCODE)) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("moisture1");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
     }
   }
-  void loop() {
-    if (!client.connected()) {
-      reconnect();
-    }
-    client.loop();
+}
 
-    long now = millis();
-    if (now - lastMsg > 5000) {
-      lastMsg = now;
-
-      humidity = random(10, 50);
-
-      // Convert the value to a char array
-      char humString[8];
-      dtostrf(humidity, 1, 2, humString);
-      Serial.print("Humidity: ");
-      Serial.println(humString);
-      client.publish("esp32/humidity", humString);
-    }
+void loop() {
+  if (!client.connected()) {
+    reconnect();
   }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+
+    humidity = random(10, 50);
+
+    // Convert the value to a char array
+    char humString[8];
+    dtostrf(humidity, 1, 2, humString);
+    Serial.print("Humidity: ");
+    Serial.println(humString);
+    client.publish("moisture1", humString);
+  }
+}
